a9704f27e54e72d494a2ab9941cd8514e15f49ae
Zignuts Technolab <ztlab102@Zignutss-Mac-mini.local>
2023-01-03 18:40:59 +0530

    botton navigation bar and drawer

--- lib/dash_board_screen.dart
+++ lib/dash_board_screen.dart
@@ -6,6 +6,33 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  int indx = 0;
+
+  List<Widget> screens = [
+    Center(
+      child: Text(
+        'Home',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text('Vendors',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    ),
+    Center(
+      child: Text('Lists',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    ),
+    Center(
+      child: Text('Categories',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    ),
+    Center(
+      child: Text('More',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +40,64 @@ class _DashBoardState extends State<DashBoard> {
         title: Text('Dashbooard'),
         centerTitle: true,
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: indx,
+          onTap: (value) {
+            setState(() {
+              indx = value;
+            });
+          },
+          unselectedItemColor: Colors.grey,
+          fixedColor: Colors.blue,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Vendors'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Lists'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category), label: 'Categories'),
+            BottomNavigationBarItem(icon: Icon(Icons.more), label: 'More'),
+          ]),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text(
+                  'Nisarg Chaniyara',
+                  style: TextStyle(fontSize: 15),
+                ),
+                accountEmail: Text('nisargc@zignuts.com')),
+            ListTile(
+              leading: const Icon(Icons.book),
+              title: const Text(' Payment method'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.workspace_premium),
+              title: const Text(' Address '),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text(' Household'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('LogOut'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: screens[indx],
     );
   }
 }
